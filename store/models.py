22329133c39073ee389f3362b85805e9c1fcd2a2
kayhan-kashi <kayhan.kashi@gmail.com>
2023-07-11 08:45:20 +0330

    zip added

--- store/models.py
+++ store/models.py
@@ -44,6 +44,13 @@ class Customer(models.Model):
      phone = models.CharField(max_length=255)
      birth_date = models.DateField(null=True)
      membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE, )
+     
+     class Meta:
+          db_table = 'store_customers'
+          indexes = [
+               models.Index(fields=['last_name', 'first_name'])
+          ]
+     
 
 
 class Order(models.Model):
@@ -72,7 +79,7 @@ class Address(models.Model):
      city = models.CharField(max_length=255)
      # customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key= True )
      customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-
+     zip = models.CharField(max_length=50, null=True)
 
 
 class Cart(models.Model):
